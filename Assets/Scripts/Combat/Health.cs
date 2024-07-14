6c14df5c5c0d17235a14e0021c761f7bda3805f4
kekekuli <kekekuliobj@gmail.com>
2024-07-14 21:05:29 +0800

    Death Splatter

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _splatterPrefab;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
@@ -20,7 +21,14 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
+            SpawnSplatterPrefab();
             Destroy(gameObject);
         }
+    }
+    private void SpawnSplatterPrefab() {
+        GameObject newSplatterPrefab = Instantiate(_splatterPrefab, transform.position, Quaternion.identity);
+        SpriteRenderer splatterSpriteRenderer = newSplatterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        splatterSpriteRenderer.color = colorChanger.DefaultColor;
     }
 }
