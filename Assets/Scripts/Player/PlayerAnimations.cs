7395fdfa4e0bedf96b5610830d7cf23764573a70
kekekuli <kekekuliobj@gmail.com>
2024-07-21 11:07:43 +0800

    Cowboy Hat

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -9,6 +9,8 @@ public class PlayerAnimations : MonoBehaviour
     [SerializeField] private float _tiltAngle = 20f;
     [SerializeField] private float _tiltSpeed = 5f;
     [SerializeField] private Transform _playerSpriteTransform;
+    [SerializeField] private Transform _cowboyHatTransform;
+    [SerializeField] private float _cowboyHatTiltModifer = 2f;
     
     private void Update() {
         DetectMoveDust();
@@ -36,11 +38,17 @@ public class PlayerAnimations : MonoBehaviour
         else
             targetAngle = 0;
         
+        // Player Sprite Render
         Quaternion currentRotation = _playerSpriteTransform.rotation;
         Vector3 eulerAngles = currentRotation.eulerAngles;
         Quaternion targetRotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, targetAngle);
-
         _playerSpriteTransform.rotation = Quaternion.Slerp(currentRotation, targetRotation, _tiltSpeed * Time.deltaTime);
+        
+        // Cowboy Hat Render
+        Quaternion currentHatRotation = _cowboyHatTransform.rotation;
+        Vector3 hatEulerAngles = currentHatRotation.eulerAngles;
+        Quaternion targetHatRotation = Quaternion.Euler(hatEulerAngles.x, hatEulerAngles.y, -targetAngle / _cowboyHatTiltModifer);
+        _cowboyHatTransform.rotation = Quaternion.Slerp(currentHatRotation, targetHatRotation, _tiltSpeed * +_cowboyHatTiltModifer * Time.deltaTime);
     }
     
 }
