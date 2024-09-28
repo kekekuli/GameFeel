c9e2f9feff41dba77d6ce571bd3b8442c5921a2e
kekekuli <kekekuliobj@gmail.com>
2024-09-28 14:56:51 +0800

    Managing Sound Values

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private float _masterVolume = 1f;
     [SerializeField] private SoundSO _gunShoot;
     [SerializeField] private SoundSO _playerJump;
 
@@ -17,17 +16,36 @@ public class AudioManager : MonoBehaviour
         Gun.OnShoot -= Gun_OnShoot;
         PlayerController.OnJump -= PlayerController_OnJump;
     }
-    private void PlaySound(SoundSO sound){
-        var soundObj = new GameObject("Temp Audio Source");
-        var audioSource = soundObj.AddComponent<AudioSource>(); 
-        audioSource.clip = sound.Clip;
+    private void SoundToPlay(SoundSO sound){
+        var clip = sound.Clip;
+        var volume = sound.Volume * _masterVolume;
+        var pitch = sound.Pitch;
+        var loop = sound.Loop;
+
+        if(sound.RandomizePitch){
+            pitch += Random.Range(-sound.RanomPitchRangeModifier, sound.RanomPitchRangeModifier);
+        }
+
+        PlaySound(clip, volume, pitch, loop);
+    }
+
+    private void PlaySound(AudioClip clip, float volume, float pitch, bool loop){
+        var audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = volume * _masterVolume;
+        audioSource.pitch = pitch;
+        audioSource.loop = loop;
         audioSource.Play();
+
+        if (!loop){
+            Destroy(audioSource, clip.length);
+        }
     }
 
     private void Gun_OnShoot(){
-        PlaySound(_gunShoot);
+        SoundToPlay(_gunShoot);
     }
     private void PlayerController_OnJump(){
-        PlaySound(_playerJump); 
+        SoundToPlay(_playerJump);
     }
 }
